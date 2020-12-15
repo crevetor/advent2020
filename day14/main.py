f683a053f64d3dac013a2e5874c4c544097af625
Antoine Reversat <a.reversat@gmail.com>
2020-12-15 17:27:31 -0500

    day 14 final

--- day14/main.py
+++ day14/main.py
@@ -33,16 +33,6 @@ def part1():
 
     print(sum(list(finalmem.values())))
 
-def gen_min_maxes(masked_addr):
-    ret = []
-    parts = re.findall(r'[0-1]*[X]+[0-1]*', masked_addr)
-    for i, part in enumerate(parts):
-        min_part = int(part.replace('X', '0'), 2) << len(''.join(parts[i+1:]))
-        max_part = int(part.replace('X', '1'), 2) << len(''.join(parts[i+1:]))
-        ret.append([min_part, max_part])
-
-    return ret
-
 def gen_addrs(addr, mask):
     addr = bin(addr)[2:].zfill(len(mask))
     masked_addr = ""
@@ -52,20 +42,22 @@ def gen_addrs(addr, mask):
         if bit == '0':
             masked_addr += addr[i]
 
-    #print(f'{masked_addr}')
-    mins_maxes = gen_min_maxes(masked_addr)
+    parts = re.findall(r'[0-1]*[X][0-1]*', masked_addr)
+    assert len(''.join(parts)) == len(mask)
     addrs = []
-    combinations = 2**len(mins_maxes)
-    binlenmax = len(bin(combinations - 1)[2:])
-    for i in range(combinations):
-        sum = 0
-        for idx, j in enumerate(bin(i)[2:].zfill(binlenmax)):
-            sum += mins_maxes[idx][int(j)]
-        addrs.append(sum)
+    #print(f'M: {mask}')
+    #print(f'A: {addr}')
+    #print(f'V: {masked_addr}')
+    for i in range(2**len(parts)):
+        binrepr = bin(i)[2:].zfill(len(parts))
+        outstr = ""
+        for j, bindigit in enumerate(binrepr):
+            outstr += parts[j].replace('X', bindigit)
+        #print(f'   {outstr}')
 
-    # Generate tuple of minaddr, maxaddr
-    return [(addrs[i], addrs[i+1]) for i in range(0, len(addrs), 2)]
+        addrs.append(int(outstr, 2))
 
+    return addrs
 
 def part2():
     finalmem = {}
@@ -77,10 +69,8 @@ def part2():
             addr, val = parse_mem(mem)
             addrs = gen_addrs(addr, mask)
             for a in addrs:
-                for i in range(a[0], a[1]+1):
-                    finalmem[i] = val
+                finalmem[a] = val
 
-
-    print(sum(list(finalmem.values())))
+    print(sum(finalmem.values()))
 
 part2()
