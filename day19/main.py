33432cd00e9c34851ae8eb3f4af97d3ae43bf4e1
Antoine Reversat <a.reversat@gmail.com>
2020-12-20 19:26:30 -0500

    Day 19 part1

---
+++ day19/main.py
@@ -0,0 +1,68 @@
+import re
+
+with open("input3", "r") as fd:
+    lines = [line.strip() for line in fd.readlines()]
+
+class Rule:
+
+    def __init__(self, idx, rule):
+        self.idx = idx
+        self.children = {}
+        if '"' in rule:
+            self.rule = rule.strip('"')
+            self.hasDependency = False
+        else:
+            self.rule = rule
+            self.hasDependency = True
+    
+    @property
+    def dependencies(self):
+        return set(map(int, re.findall(r'\d+', self.rule)))
+
+    def addChildRule(self, rule):
+        self.children[rule.idx] = rule
+
+    def render(self):
+        rendered = "(" if self.hasDependency else ""
+        for char in self.rule.split(' '):
+            if char.isdigit():
+                if int(char) != self.idx:
+                    rendered += self.children[int(char)].render()
+                elif int(char) == 8:
+                    rendered += '+'
+                elif int(char) == 11:
+                    rendered += self.children[42].render() + self.children[31].render()
+            else:
+                rendered += char
+        return rendered + ")" if self.hasDependency else rendered
+
+def parse_rules(rule_lines):
+    ruleobjs = {}
+    for line in rule_lines:
+        idx, rule = line.split(': ')
+        ruleobjs[int(idx)] = (Rule(int(idx), rule))
+
+    for rule in ruleobjs.values():
+        for dep in rule.dependencies:
+            rule.addChildRule(ruleobjs[dep])
+
+    return f"^{ruleobjs[0].render()}$"
+
+def part1():
+    rules = []
+    for breakidx, line in enumerate(lines):
+        if line == '':
+            break
+        rules.append(line)
+    
+    regex = parse_rules(rules)
+    print(regex)
+    count = 0
+    for line in lines[breakidx:]:
+        if re.fullmatch(regex, line):
+            count += 1
+            print(line)
+
+    print(f"{count} lines match")
+
+part1()
